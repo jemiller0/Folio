182af096644ee6a46964482cff4e4309b2d34c6e
Jon Miller <jemiller@uchicago.edu>
2022-02-09 16:36:03 -0600

    Display stack trace on AsyncPostBackError

--- FolioWebApplication/Default.master.cs
+++ FolioWebApplication/Default.master.cs
@@ -13,14 +13,14 @@ namespace FolioWebApplication
                 if (Session["LabelsPermission"] != null)
                 {
                     rmi.Visible = true;
-                    rmi.Items.Insert(rmi.Items.IndexOf(rmi.Items.FindItemByText("Libraries")), new RadMenuItem("Labels", "~/Labels/Default.aspx"));
                 }
             }
         }
 
         protected void RadScriptManager1_AsyncPostBackError(object sender, System.Web.UI.AsyncPostBackErrorEventArgs e)
         {
-            RadScriptManager1.AsyncPostBackErrorMessage = (e.Exception.InnerException?.Message ?? e.Exception.Message).Replace("A task was canceled.", "Request timed out.");
+            //RadScriptManager1.AsyncPostBackErrorMessage = (e.Exception.InnerException?.Message ?? e.Exception.Message).Replace("A task was canceled.", "Request timed out.");
+            RadScriptManager1.AsyncPostBackErrorMessage = (e.Exception.InnerException ?? e.Exception).ToString();
             //Server.ClearError();
         }
     }
